bbe2628993ea73a1a48e6be9c5986eb5eb98a6cd
PeterT137 <trong6packs@gmail.com>
2025-06-25 16:33:22 +0700

    Delete unnecessary project SPORTZONE_SEP490_G83

--- SPORTZONE_SEP490_G83/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SPORTZONE_SEP490_G83.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
